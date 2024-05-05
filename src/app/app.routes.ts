0f813ae86d377b97eb230d74f8558ececf3733ba
DNC202 <nguyencongdanh2002@gmail.com>
2024-05-05 12:25:02 +0700

    add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ToursComponent } from './pages/tours/tours.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'tours', component: ToursComponent}
+    { path: 'tours', component: ToursComponent},
+    { path: 'login', component: LoginComponent}
 ];
